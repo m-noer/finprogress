b8d99de6154f485e77a8a4905919f85abd53a5af
Muhammad Noerhidayatullah <mnoer@outlook.com>
2018-05-28 05:15:23 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# finprogress
+# finpropwa
---
+++ new.js
@@ -0,0 +1,17 @@
+var lapor = [
+  {
+    img:'images/Alex.jpg',
+    name: 'Alex Sunarto',
+    time: '2 minutes ago',
+    title: 'Situs yang diblokir Pemerintah Masih Bisa Di Buka',
+    content: 'Kementerian Komunikasi dan Informatika.Situs-situs yang telah diblokir oleh pemerintah (pornografi, radikalisme dan sebagainya) ternyata masih bisa diakses dengan menggunakan browser Opera Mini.'
+  },
+  {
+    img:'images/Alex.jpg',
+    name: 'Alex Sunarto',
+    time: '2 minutes ago',
+    title: 'Situs yang diblokir Pemerintah Masih Bisa Di Buka',
+    content: 'Kementerian Komunikasi dan Informatika.Situs-situs yang telah diblokir oleh pemerintah (pornografi, radikalisme dan sebagainya) ternyata masih bisa diakses dengan menggunakan browser Opera Mini.'
+  }
+
+]
